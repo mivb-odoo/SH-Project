486516840c0979972206eec1cd3214c046190d27
mivb-odoo <mivb@odoo.com>
2022-06-13 13:13:12 -0400

    Odoo Academy Training

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': 'Academy app to manage training',
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    'author': 'Mihir',
+    'website': 'https://www.odoo.com',
+    'category': 'Training week 2',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+}
